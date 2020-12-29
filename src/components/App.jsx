4ce6454f043ac81e4c0d93062e27686c97f8f35a
Abdelmawla Souat <abdelmawla.souat@gmail.com>
2020-12-29 17:56:15 +0100

    Add states to Ip Address Data Box

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,82 +4,79 @@
  * @author Abdelmawla Souat <abdelmawla.souat@gmail.com>
  *
  * Created at     : 2020-12-26 10:48:01
- * Last modified  : 2020-12-28 21:14:28
+ * Last modified  : 2020-12-29 17:47:49
  */
 
 import { useState, useEffect } from 'react'
+import { Box, ThemeProvider } from '@material-ui/core'
 import axios from 'axios'
-import { Box, ThemeProvider, makeStyles } from '@material-ui/core'
-
 import theme from '../theme'
 import patternBg from '../images/pattern-bg.png'
-
 import PatternBackground from './PatternBackground'
 import IPAddressContainer from './IPAddressContainer'
 import Map from './Map'
 
-const useStyles = makeStyles({
-	app: { height: '100vh' },
-})
+const API_KEY = process.env.REACT_APP_IPIFY_KEY
 
 function App() {
-	const [position, setPosition] = useState({
-		x: 0,
-		y: 0,
-	})
-	const [ipAddress, setIpAddress] = useState('')
-	const [location, setLocation] = useState({})
-	const [isp, setIsp] = useState('')
-	const [zoom, setZoom] = useState(1)
-	const classes = useStyles()
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [ipAddress, setIpAddress] = useState('')
+  const [isp, setIsp] = useState('Unknown')
+  const [timezone, setTimeZone] = useState('Unknown')
+  const [zoom, setZoom] = useState(1)
+  const [location, setLocation] = useState({
+    city: 'Unknown',
+    postalCode: 'Unknown',
+  })
 
-	useEffect(() => {
-		if ('geolocation' in navigator) {
-			navigator.geolocation.getCurrentPosition(() => {
-				axios
-					.get('https://geoip-db.com/json/')
-					.then((json) => json.data.IPv4)
-					.then((ipAddr) => {
-						axios
-							.get(
-								`https://geo.ipify.org/api/v1?apiKey=at_PhQZKOdzMRSwes9SIULzywxzAwVeN&ipAddress=${ipAddr}`
-							)
-							.then((json) => {
-								const { ip, location: loc, isp: ispValue } = json.data
+  function getLocationData(ipAddr) {
+    axios
+      .get(`https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ipAddr}`)
+      .then((response) => {
+        const { ip, location: loc, isp: ispValue } = response.data
 
-								console.log(json.data)
-								setIpAddress(ip)
-								setPosition({ x: loc.lng, y: loc.lat })
-								setLocation({
-									city: loc.city,
-									postalCode: loc.postalCode,
-									timezone: loc.timezone,
-								})
-								setIsp(ispValue)
-							})
-							.catch((err) => console.log(err))
-					})
-					.catch((err) => console.log(err))
-			})
-		}
-	}, [])
+        setLocation({ city: loc.city, postalCode: loc.postalCode })
+        setPosition({ x: loc.lng, y: loc.lat })
+        setTimeZone(loc.timezone)
+        setIpAddress(ip)
+        setIsp(ispValue)
+      })
+      .catch((err) => console.log(err))
+  }
 
-	return (
-		<ThemeProvider theme={theme}>
-			<Box className={classes.app}>
-				{`${position.x} - ${position.y} $$$ ip : ${ipAddress}  $`}
-				{`isp: ${isp} - location: ${location} `}
-				<PatternBackground src={patternBg} />
-				<IPAddressContainer />
-				<Map
-					position={position}
-					handlePosition={() => setPosition(position)}
-					handleZoom={() => setZoom(zoom)}
-					zoom={zoom}
-				/>
-			</Box>
-		</ThemeProvider>
-	)
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(() => {
+        axios
+          .get('https://geoip-db.com/json/')
+          .then((response) => response.data.IPv4)
+          .then((ipAddr) => getLocationData(ipAddr))
+          .catch((err) => console.log(err))
+      })
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box className="App" style={{ height: '100vh' }}>
+        {`${position.x} - ${position.y} $$$ ip : ${ipAddress}  $`}
+        {`isp: ${isp}`}
+        <PatternBackground src={patternBg} />
+        <IPAddressContainer
+          ipAddress={ipAddress}
+          location={location}
+          timezone={timezone}
+          isp={isp}
+        />
+        <Map
+          position={position}
+          handlePosition={() => setPosition(position)}
+          handleZoom={() => setZoom(zoom)}
+          zoom={zoom}
+        />
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App
--- src/components/IPAdressData.jsx
+++ src/components/IPAdressData.jsx
@@ -4,75 +4,77 @@
  * @author Abdelmawla Souat <abdelmawla.souat@gmail.com>
  *
  * Created at     : 2020-12-26 12:15:55
- * Last modified  : 2020-12-26 21:19:30
+ * Last modified  : 2020-12-29 17:51:58
  */
 
 import { Box, Card, Typography, makeStyles } from '@material-ui/core'
 import clsx from 'clsx'
 
 const useStyles = makeStyles((theme) => ({
-	ipAddressDataCard: { borderRadius: '15px' },
-	ipAddressDataBox: {
-		display: 'flex',
-		padding: '1rem',
-		[theme.breakpoints.only('xs')]: {
-			flexDirection: 'column',
-			padding: '0',
-		},
-	},
-	item: {
-		margin: '2.7rem',
-		paddingLeft: '2.2rem',
-		[theme.breakpoints.only('xs')]: {
-			margin: '1rem 1.5rem',
-			paddingLeft: '0',
-			textAlign: 'center',
-		},
-	},
-	borderLeft: {
-		borderLeft: '1px solid lightgrey',
-		[theme.breakpoints.only('xs')]: { borderLeft: '0' },
-	},
-	categorie: {
-		color: 'grey',
-		fontSize: '0.75rem',
-		letterSpacing: '2px',
-		fontWeight: 'bold',
-	},
-	data: {
-		fontSize: '2rem',
-		fontWeight: 'bold',
-		[theme.breakpoints.only('xs')]: { fontSize: '1.5rem' },
-	},
+  ipAddressDataCard: { borderRadius: '15px' },
+  ipAddressDataBox: {
+    display: 'flex',
+    padding: '1rem',
+    [theme.breakpoints.only('xs')]: {
+      flexDirection: 'column',
+      padding: '0',
+    },
+  },
+  item: {
+    margin: '2.7rem',
+    paddingLeft: '2.2rem',
+    [theme.breakpoints.only('xs')]: {
+      margin: '1rem 1.5rem',
+      paddingLeft: '0',
+      textAlign: 'center',
+    },
+  },
+  borderLeft: {
+    borderLeft: '1px solid lightgrey',
+    [theme.breakpoints.only('xs')]: { borderLeft: '0' },
+  },
+  categorie: {
+    color: 'grey',
+    fontSize: '0.75rem',
+    letterSpacing: '2px',
+    fontWeight: 'bold',
+  },
+  data: {
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    [theme.breakpoints.only('xs')]: { fontSize: '1.5rem' },
+  },
 }))
 
-function IPAdressData() {
-	const classes = useStyles()
+function IPAdressData({ ipAddress, location, timezone, isp }) {
+  const classes = useStyles()
 
-	return (
-		<Card className={classes.ipAddressDataCard}>
-			<Box className={classes.ipAddressDataBox}>
-				<Box className={classes.item}>
-					<Typography className={classes.categorie}>IP ADDRESS</Typography>
-					<Typography className={classes.data}>192.212.174.101</Typography>
-				</Box>
+  return (
+    <Card className={classes.ipAddressDataCard}>
+      <Box className={classes.ipAddressDataBox}>
+        <Box className={classes.item}>
+          <Typography className={classes.categorie}>IP ADDRESS</Typography>
+          <Typography className={classes.data}>{ipAddress}</Typography>
+        </Box>
 
-				<Box className={clsx(classes.borderLeft, classes.item)}>
-					<Typography className={classes.categorie}>LOCATION</Typography>
-					<Typography className={classes.data}>Brooklyn, NY</Typography>
-				</Box>
+        <Box className={clsx(classes.borderLeft, classes.item)}>
+          <Typography className={classes.categorie}>LOCATION</Typography>
+          <Typography className={classes.data}>
+            {`${location.city}, ${location.postalCode}`}
+          </Typography>
+        </Box>
 
-				<Box className={clsx(classes.borderLeft, classes.item)}>
-					<Typography className={classes.categorie}>TIMEZONE</Typography>
-					<Typography className={classes.data}>UTC -05.00</Typography>
-				</Box>
+        <Box className={clsx(classes.borderLeft, classes.item)}>
+          <Typography className={classes.categorie}>TIMEZONE</Typography>
+          <Typography className={classes.data}>{`UTC ${timezone}`}</Typography>
+        </Box>
 
-				<Box className={clsx(classes.borderLeft, classes.item)}>
-					<Typography className={classes.categorie}>ISP</Typography>
-					<Typography className={classes.data}>SpaceX</Typography>
-				</Box>
-			</Box>
-		</Card>
-	)
+        <Box className={clsx(classes.borderLeft, classes.item)}>
+          <Typography className={classes.categorie}>ISP</Typography>
+          <Typography className={classes.data}>{isp}</Typography>
+        </Box>
+      </Box>
+    </Card>
+  )
 }
 export default IPAdressData
